cda18a8311e4858337d4b54996e2b2a7beb74d50
Austin Hodge <ahodge114@rams.wssu.edu>
2017-11-21 15:38:30 -0500

    Added comments and clean code a bit

--- index.js
+++ index.js
@@ -1,87 +1,69 @@
 #! /usr/bin/env nodejs
-'use strict';
 
-var Cron = require('cron');
-var Moment = require('moment-timezone');
-let https = require('https');
-var fs = require('fs');
-var request = require('request');
+const cron = require('cron');
+const moment = require('moment-timezone');
+const https = require('https');
+const fs = require('fs');
+const request = require('request');
 const shell = require('shelljs');
 
-var subscriptionKey = '65b6410d6cd94dc99b00e719c1dc5b40';
-var searchQueries = ['Windows Spotlight Wallpapers', 'Most Beautiful Landscapes', 'Most Breathtaking Views'];
-    //'Greatest Architectural Buildings hd', 
+const subscriptionKey = '65b6410d6cd94dc99b00e719c1dc5b40';
+const searchQueries = ['Windows Spotlight Wallpapers', 'Most Beautiful Landscapes', 'Most Breathtaking Views', 'best landscape photography'];
 
-var runningJob = new Cron.CronJob(
-    '0 */45 * * * *',  // s(0-59) m(0-59) h(0-23) d(1-31) m(0-11) w(0-6)
+var runningJob = new cron.CronJob(
+    '0 */45 * * * *',  // s(0-59) m(0-59) h(0-23) d(1-31) m(0-11) w(0-6) Run every 45 minutes of the hour ex. 2:00, 2:45, 3:00, etc
     function(){
+        // Job started so set background
         chooseBackground();
     },
     function(){
         // Job has stopped
     },
-    false, // Start job later
-    Moment.tz.guess()
+    false, // Start the job later
+    moment.tz.guess()
 );
 function chooseBackground(){
+    // Bing Search API Endpoints
     let host = 'api.cognitive.microsoft.com';
     let path = '/bing/v7.0/images/search';
     let chosenImage = '';
+
     let response_handler = function (response) {
         let body = '';
         response.on('data', function (d) {
+            // Retrieve the json data
             body += d;
         });
         response.on('end', function () {
-            //console.log('\nRelevant Headers:\n');
-            //for (var header in response.headers)
-                // header keys are lower-cased by Node.js
-                //if (header.startsWith("bingapis-") || header.startsWith("x-msedge-"))
-                  //   console.log(header + ": " + response.headers[header]);
-            
+            // Convert the data to JSON object
             let images = JSON.parse(body)
             let result = JSON.stringify(images, null, '  ');
             
-            var links = "";
-            let paths = [];
+            let imgURLS = [];
             for (let img in images.value){
-                links += images.value[img].contentUrl + '\n';
-                paths.push(images.value[img].contentUrl);
+                imgURLS.push(images.value[img].contentUrl);
             }
-            var randomIndex = Math.floor(Math.random() * paths.length);
-            chosenImage = paths[randomIndex];
-            while(!chosenImage.endsWith('.jpg')){
-                randomIndex = Math.floor(Math.random() * paths.length);
-                chosenImage = paths[randomIndex];
-            }
+            // Choose a random url from the image urls
+            var randomIndex = Math.floor(Math.random() * imgURLS.length);
+            chosenImage = imgURLS[randomIndex];
             console.log(chosenImage);
-            
             
             var download = function(uri, filename, callback){
                 request.head(uri, function(err, res, body){
-                  //console.log('content-type:', res.headers['content-type']);
-                  //console.log('content-length:', res.headers['content-length']);
-              
+                  console.log('content-type:', res.headers['content-type']);
+                  console.log('content-length:', res.headers['content-length']);
+                  // write image data to file and return callback on close/exit
                   request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
                 });
               };
-              
+              // download the image and set ubuntu desktop background once done
               download(chosenImage, 'chosenImage.jpg', function(){
                 console.log('...Setting Background...');
                 let pictureFilePath = 'file://'+__dirname + '/chosenImage.jpg';
-                console.log('gsettings set org.gnome.desktop.background picture-uri '+ pictureFilePath+'');
                 shell.exec('gsettings set org.gnome.desktop.background picture-uri '+pictureFilePath+'', function(stdout, stderr, std){
-                    console.log(std);
+                    console.log(stderr);
                 });
             });
-            
-            fs.writeFile("response.txt", links, function(err) {
-                if(err) {
-                    return console.log(err);
-                }
-            
-                console.log("...Saving links to file...");
-            }); 
         });
         response.on('error', function (e) {
             console.log('Error: ' + e.message);
@@ -101,6 +83,7 @@ function chooseBackground(){
           let req = https.request(request_params, response_handler);
           req.end();
       };
+      // Search bing with random search query from list
       bing_web_search(searchQueries[Math.floor(Math.random() * searchQueries.length)]);
 }
 
